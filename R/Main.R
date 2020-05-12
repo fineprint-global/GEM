b57ed9c8dfc6fd233e76c48d250ae7755447f197
Hanspeter85 <hanspeter.wieland@wu.ac.at>
2020-05-12 23:15:18 +0200

    The MRIO object is now a global variable.

--- R/Main.R
+++ R/Main.R
@@ -2,8 +2,8 @@
 # This is the main script for the calculus where all paths to folders and data
 # are defined and all functions are executed
 
-pack <- c('tidyverse','openxlsx')  # Define packages to be used
-install.packages( pack )           # Install packages
+#pack <- c('tidyverse','openxlsx')  # Define packages to be used
+#install.packages( pack )           # Install packages
 library( "tidyverse" )             # Load packages
 library( "openxlsx" )
 
@@ -21,7 +21,13 @@ lapply(fun,source)                                         # Load all functions
 
 remove(github,fun)         # Remove objects not needed anymore
 
-MRIO <- Compile_EXIOBASE(2008,"PxP")
+MRIO <<- Compile_EXIOBASE(2008,"PxP")   # Create EXIOBASE MRIO with material extension
 
+
+
+
+
+
+rowSums(MRIO$E)
 
 
